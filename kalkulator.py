4f3d3242c19b86aedd0e2e6ef7206984a414c622
Radek Bukalski <bukalskiradek@gmail.com>
2022-02-28 20:58:00 +0100

    logging library used and variables naming modified to eng

--- kalkulator.py
+++ kalkulator.py
@@ -1,9 +1,12 @@
-#kalkulator v.1
+#kalkulator v.2
 
-działanie = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: \n')
-num1 = int(input('Podaj pierwszą liczbe:'))
-num2 = int(input('Podaj drugą liczbe:'))
+import logging
 
+
+operation = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: \n'))
+num1 = float(input('Podaj skadnik 1 :'))
+num2 = float(input('Podaj skadnik 2 :'))
+logging.basicConfig(level=logging.DEBUG, format='')
 def add(num1,num2):
     return num1 + num2
 
@@ -19,15 +22,19 @@ def div(num1,num2):
     else:
         return num1 / num2
 
-if działanie == '1':
-    print (f'Wynik działania: ' , add(num1,num2))
+if operation == 1:
+    logging.debug(f'Dodaje {num1} i {num2}')
+    print (f'Wynik to : ' , add(num1,num2))
 
-if działanie == '2':
-    print (f'Wynika działania: ' , sub(num1,num2))
+if operation == 2:
+    logging.debug(f'Odemmuje {num2} od {num1}')
+    print (f'Wynik to : ' , sub(num1,num2))
 
-if działanie == '3':
-    print (f'Wynika działania: ' , mul(num1,num2))
+if operation == 3:
+    logging.debug(f'Mnoze {num1} przez {num2}')
+    print (f'Wynik to : ' , mul(num1,num2))
 
-if działanie == '4':
-    print (f'Wynik działania: ' , div(num1,num2))
+if operation == 4:
+    logging.debug(f'Dziele {num1} przez {num2}')
+    print (f'Wynik to : ' , div(num1,num2))
 
